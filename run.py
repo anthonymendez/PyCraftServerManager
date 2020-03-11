e5e1a66481e30f0fed72401628916e821b252983
Anthony Mendez <anthonymendez9@gmail.com>
2020-03-11 19:59:56 -0400

    Added comments

--- run.py
+++ run.py
@@ -5,11 +5,13 @@ from Configuration.ServerPropertiesHandler import ServerPropertiesHandler
 
 # Server Runner
 ServerRunner = VanillaServerRunner("../Vanilla_Server_Basic/")
+
 # Server Properties Handler
 ServerPropertiesHandler = ServerPropertiesHandler(ServerRunner)
 # Whitelist Handler
 WhitelistHandler = WhitelistHandler(ServerRunner)
 
+# Set IP and Server Port
 ServerPropertiesHandler.set_property("server-ip", "0.0.0.0")
 ServerPropertiesHandler.set_property("server-port", "25565")
 
