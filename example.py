1afc2aa92479da6da2649e25cfadcb9ff10918dc
Anthony Mendez <anthonymendez9@gmail.com>
2020-04-01 16:31:52 -0400

    Updated run.py and example.py

--- example.py
+++ example.py
@@ -1,27 +1,21 @@
 from ServerRunner.VanillaServerRunner import VanillaServerRunner
-from Configuration.WhitelistHandler import WhitelistHandler
-from Configuration.ServerPropertiesHandler import ServerPropertiesHandler
 from MiscFunctions.MojangAPI import player_to_uuid
 
 from threading import Thread
 
-player_to_uuid("Tony_De_Tiger")
+# player_to_uuid Handler
+print(player_to_uuid("Tony_De_Tiger"))
 
 # Server Runner
 VanillaServerRunnerTest = VanillaServerRunner("../Vanilla_Server_Basic/")
 
 # Server Properties Handler
-ServerPropertiesHandlerTest = ServerPropertiesHandler(VanillaServerRunnerTest)
+ServerPropertiesHandlerTest = VanillaServerRunnerTest.ServerPropertiesHandler
 ServerPropertiesHandlerTest.set_property("server-ip", "127.0.0.1")
 ServerPropertiesHandlerTest.set_property("server-port", "5050")
 
-# player_to_uuid Handler
-print(player_to_uuid("Tony_De_Tiger"))
-
 # Whitelist Handler
-WhitelistHandlerTest = WhitelistHandler(VanillaServerRunnerTest)
-
-ServerRunnerThread = Thread(target=VanillaServerRunnerTest.run())
+WhitelistHandlerTest = VanillaServerRunnerTest.WhitelistHandler
 
 # VanillaServerRunnerTest.run()
 server_process = VanillaServerRunnerTest.server_process
--- run.py
+++ run.py
@@ -1,18 +1,14 @@
 from ServerRunner.VanillaServerRunner import VanillaServerRunner
-from Configuration.WhitelistHandler import WhitelistHandler
-from Configuration.ServerPropertiesHandler import ServerPropertiesHandler
 
 
 # Server Runner
 ServerRunner = VanillaServerRunner("../Vanilla_Server_Basic/")
 
 # Server Properties Handler
-ServerPropertiesHandler = ServerPropertiesHandler(ServerRunner)
+ServerPropertiesHandler = ServerRunner.ServerPropertiesHandler
 # Whitelist Handler
-WhitelistHandler = WhitelistHandler(ServerRunner)
+WhitelistHandler = ServerRunner.WhitelistHandler
 
 # Set IP and Server Port
 ServerPropertiesHandler.set_property("server-ip", "0.0.0.0")
 ServerPropertiesHandler.set_property("server-port", "25565")
-
-ServerRunner.run()
