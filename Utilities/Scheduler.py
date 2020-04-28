640b39a37e70a2b50e496c915312204de8208931
Anthony Mendez <anthonymendez9@gmail.com>
2020-04-27 20:36:48 -0400

    #11 Scheduler class uses input handler fn ptr

--- Utilities/Scheduler.py
+++ Utilities/Scheduler.py
@@ -9,13 +9,17 @@ class Scheduler():
 
     list_file_name = "scheduler.csv"
 
-    def __init__(self, main_directory, server_directory):
+    def __init__(self, main_directory, server_directory, input_handler):
         """
-        Initializes Scheduler for server by tying it to a Server Directory and creating the scheduler file.
+        Initializes Scheduler for server by:\n
+        \tTying it to a Server Directory.\n
+        \tTying it to the server runner's input handler function
+        \tCreating the scheduler file.
         """
         # Store all current server properties
         self.main_directory = main_directory
         self.server_directory = server_directory
+        self.input_handler = input_handler
         self.list_file_path = os.path.join(self.server_directory, self.list_file_name)
         # Create scheduler.list file if it doesn't exist and create headers for it
         if not os.path.exists(self.list_file_path):
