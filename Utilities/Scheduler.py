3120f0ef188b57587542f7574efc8434ed8891d6
Anthony Mendez <anthonymendez9@gmail.com>
2020-04-28 04:55:21 -0400

    #11 Switched to AP Scheduler, using Cron like scheduling

--- Utilities/Scheduler.py
+++ Utilities/Scheduler.py
@@ -4,7 +4,16 @@ import pickle
 import re
 
 from apscheduler.triggers import cron
+from apscheduler.schedulers.background import BackgroundScheduler
 from threading import Thread
+
+# TODO Move to it's own module
+def is_valid_date(date_str):
+    try:
+        datetime.datetime.strptime(date_str, "%Y-%m-%d")
+        return True
+    except ValueError:
+        return False
 
 # TODO: Move to it's own module
 def is_int(s):
@@ -35,19 +44,51 @@ class Scheduler():
         self.server_directory = server_directory
         self.input_handler = input_handler
         self.list_file_path = os.path.join(self.server_directory, self.list_file_name)
+        self.sched = BackgroundScheduler()
         # Create scheduler.list file if it doesn't exist and create headers for it
         if not os.path.exists(self.list_file_path):
             list_file = open(self.list_file_path, 'w')
             list_file.write("")
             list_file.close()
     
-    def create_scheduled_command(self, command, scheduler_commands):
+    def create_scheduled_command(self, command, cron_string):
         """
         Creates scheduled command using the given scheduler command string.\n
         """
         # Check for empty strings
-        if command == None or len(command) == 0 or scheduler_commands == None or len(scheduler_commands) == 0:
+        if command == None or len(command) == 0 or cron_string == None or len(cron_string) == 0:
             return False
 
-        # Check if the first argument is an integer. If so it is the interval for every command.
-        
+        # Check if it fits the amount arguments needed for cron
+        cron_list = cron_string.splits(" ")
+        if (len(cron_list) < 11 or len(cron_list > 12)):
+            return False
+
+        year = cron_list[0]
+        month = cron_list[1]
+        day = cron_list[2]
+        week = cron_list[3]
+        day_of_week = cron_list[4]
+        hour = cron_list[5]
+        minute = cron_list[6]
+        second = cron_list[7]
+        start_date = cron_list[8]
+        end_date = cron_list[9]
+        timezone = cron_list[10]
+
+        # Check if jitter is exists
+        jitter = None
+        if (len(cron_list) == 12):
+            jitter = cron_list[11]
+
+        # Try to schedule command like a cron task
+        try:
+            self.sched.add_job(self.input_handler, "cron", args=[command], 
+                            year=year, month=month, day=day, week=week, 
+                            day_of_week=day_of_week, hour=hour, minute=minute, 
+                            second=second, start_date=start_date, end_date=end_date, 
+                            timezone=timezone, jitter=jitter)
+            return True
+        except Exception as e:
+            print(e)
+            return False
