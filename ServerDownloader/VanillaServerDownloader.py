9361f254019c09e6752c816ae8922dbed10deb8a
Anthony Mendez <anthonymendez9@gmail.com>
2020-04-28 00:29:10 -0400

    #16 Added a piece to download all minecraft server jars

--- ServerDownloader/VanillaServerDownloader.py
+++ ServerDownloader/VanillaServerDownloader.py
@@ -29,7 +29,10 @@ print("Getting download links")
 # Get download link from each page
 
 thread_q = []
-download_links = []
+versions = []
+download_links = {}
+
+# Function to get Mojang server jar download link
 
 def get_page_download_links(page):
     response = requests.get(page)
@@ -38,7 +41,13 @@ def get_page_download_links(page):
 
     a_container = soup.find_all("a", attrs={"class": "button"})
 
-    download_links.append({page[32::]: a_container[0]["href"]})
+    # Do not add if client is in link
+
+    if not "client" in a_container[0]["href"]:
+        versions.append(page[32::])
+        download_links.update({page[32::]: a_container[0]["href"]})
+
+# Start a thread for each page to get the Mojang server jar download link 
 
 for i, page in enumerate(download_pages):
     print("Page %d/%d" % (i, len(download_pages)))
@@ -46,12 +55,38 @@ for i, page in enumerate(download_pages):
     dp.start()
     thread_q.append(dp)
 
+# Wait for each thread to finish
+
 for t in thread_q:
     t.join()
 
+thread_q.clear()
+
+# Filter out links with client jars (these versions dont have a server jar)
+
 print("Pages downloaded: " + str(len(download_links)))
 for link in download_links:
-    if "client" in link:
-        download_links.remove(link)
     print(link)
-print("Pages final: " + str(len(download_links)))
+
+# Make folder to download all server jars
+
+if not os.path.exists("server_jars"):
+    os.mkdir("server_jars")
+
+def get_server_jar(version, link):
+    file = requests.get(link)
+    open(os.path.join("server_jars", str(version) + ".jar"), "wb").write(file.content)
+
+for version in versions:
+    link = download_links[version]
+    dl_jar = Thread(target=get_server_jar, args=[version, link])
+    dl_jar.start()
+    thread_q.append(dl_jar)
+
+
+print("Waiting for downloads to finish")
+
+for t in thread_q:
+    t.join()
+
+print("Download done")
