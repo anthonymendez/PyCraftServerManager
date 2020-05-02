8f01619b7deeee44def2308c2d3eccd025884fe7
Anthony Mendez <anthonymendez9@gmail.com>
2020-05-02 05:17:42 -0400

    #22 Got string to print on the last line and accept one character reading

--- curses_test.py
+++ curses_test.py
@@ -2,8 +2,17 @@ import curses
 import time
 
 screen = curses.initscr()
-str_ = "last line"
-screen.addstr(str(screen.getmaxyx()), str_)
-# screen.clear()
+screen.clear()
 
-time.sleep(3)
+curses.start_color()
+curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
+last_line = "last line"
+y = int(screen.getmaxyx()[0])
+x = int(screen.getmaxyx()[1])
+screen.addstr(y-1, 0, last_line, curses.color_pair(1))
+screen.refresh()
+k = screen.getch()
+
+time.sleep(10)
