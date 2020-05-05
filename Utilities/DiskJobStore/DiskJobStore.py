a622a840b4b4bad31d5f1bb3df72ca17aecfa612
Anthony Mendez <anthonymendez9@gmail.com>
2020-05-05 19:22:42 -0400

    #26 Finished DiskJobStore

--- Utilities/DiskJobStore/DiskJobStore.py
+++ Utilities/DiskJobStore/DiskJobStore.py
@@ -1,6 +1,7 @@
 import pickle
 import os
 
+from threading import Lock
 from apscheduler.jobstores.memory import MemoryJobStore
 
 class DiskJobStore(MemoryJobStore):
@@ -16,28 +17,21 @@ class DiskJobStore(MemoryJobStore):
         self.disk_dir = disk_dir
         self.jobs_file_path = os.path.join(self.disk_dir, jobs_file_name)
         self.jobs_index_file_path = os.path.join(self.disk_dir, jobs_index_file)
-        
+        self.disk_lock = Lock()
         # Create directory if it has not been created.
         if not os.path.exists(self.disk_dir):
             os.mkdir(self.disk_dir)
 
-        # Create file if it has not been created.
-        if not os.path.exists(self.jobs_file_path):
-            open(file=self.jobs_file_path, mode="w").close()
-        # If file does exist, load values from it.
-        else:
-            self._jobs = pickle.load(open(file=self.jobs_index_file_path, mode="r"))
-
-        # Same procedure here
-        if not os.path.exists(self.jobs_index_file_path):
-            open(file=self.jobs_index_file_path, mode="w").close()
-        else:
-            self._jobs_index = pickle.load(open(file=self.jobs_index_file_path, mode="r"))
+        # Load from jobs and jobs_index from disk.
+        self._load_from_disk()
 
 
     def lookup_job(self, job_id):
         """
-        Look up stored job. Retrieves from memory.\n
+        Look up stored job.
+
+        Retrieves from memory.
+
         Returns `None` if not found.
         """
         return super().lookup_job(job_id)
@@ -45,54 +39,105 @@ class DiskJobStore(MemoryJobStore):
     def get_due_jobs(self, now):
         """
         Get list of jobs that have `next_run_time` earlier than `now`.
+
+        Retrieves from memory.
         """
         return super().get_due_jobs(now)
 
     def get_next_run_time(self):
         """
-        Returns the earliest run time of all the jobs stored in this job store, or `None` if there are no active jobs.\n
+        Returns the earliest run time of all the jobs stored in this job store, or `None` if there are no active jobs.
+
         Retrieves from memory.
         """
         return super().get_next_run_time()
 
     def get_all_jobs(self):
         """
-        Returns a list of all jobs in this job store. The returned jobs should be sorted by next run time (ascending). Paused jobs (next_run_time == None) should be sorted last.\n
-        The job store is responsible for setting the `scheduler` and `jobstore` attributes of the returned jobs to point to the scheduler and itself, respectively.\n
+        Returns a list of all jobs in this job store. The returned jobs should be sorted by next run time (ascending). Paused jobs (next_run_time == None) should be sorted last.
+
+        The job store is responsible for setting the `scheduler` and `jobstore` attributes of the returned jobs to point to the scheduler and itself, respectively.
+
         Retrieves from memory.
         """
         return super().get_all_jobs()
 
     def add_job(self, job):
         """
-        Adds the given job to the store.\n
+        Adds the given job to the store.
+
         Store in memory and in the disk.
         """
-        return super().add_job(job)
+        super_value = super().add_job(job)
+        self._save_to_disk()
+        return super_value
 
     def update_job(self, job):
         """
         Replaces the job in the store with the given newer version.
+
+        Store in memory and in the disk.
         """
-        return super().update_job(job)
+        super_value = super().update_job(job)
+        self._save_to_disk()
+        return super_value
 
     def remove_job(self, job_id):
         """
-        Removes all jobs from this store.\n
+        Removes all jobs from this store.
+
         Removes from memory and from disk.
         """
-        return super().remove_job(job_id)
+        super_value = super().remove_job(job_id)
+        self._save_to_disk()
+        return super_value
 
     def remove_all_jobs(self):
         """
         Removes all jobs from this store.
+
+        Removes from memory and from disk.
         """
-        return super().remove_all_jobs()
+        super_value = super().remove_all_jobs()
+        self._save_to_disk()
+        return super_value
 
     def shutdown(self):
         """
         Frees any resources still bound to this job store.
+
+        Performs final save to disk before exiting.
         """
+        self._save_to_disk()
         return super().shutdown()
 
-    
+    def _load_from_disk(self):
+        """
+        Sets `self._jobs` to what currently exists in `self.jobs_file_path` file.
+
+        Sets `self._jobs_index` to what currently exists in `self.jobs_index_file_path` file.
+        """
+        with self.disk_lock:
+            # Create file if it has not been created.
+            if not os.path.exists(self.jobs_file_path):
+                open(file=self.jobs_file_path, mode="w").close()
+            # If file does exist, load values from it.
+            else:
+                self._jobs = pickle.load(open(file=self.jobs_file_path, mode="r"))
+
+            # Create file if it has not been created.
+            if not os.path.exists(self.jobs_index_file_path):
+                open(file=self.jobs_index_file_path, mode="w").close()
+            # If file does exist, load values from it.
+            else:
+                self._jobs_index = pickle.load(open(file=self.jobs_index_file_path, mode="r"))
+
+    def _save_to_disk(self):
+        """
+        Saves `self._jobs` to `self.jobs_file_path` file.
+
+        Saves `self._jobs_index` to `self.jobs_index_file_path` file.
+        """
+        with self.disk_lock:
+            pickle.dump(self._jobs, open(file=self.jobs_file_path, mode="w"))
+            pickle.dump(self._jobs_index, open(file=self.jobs_index_file_path, mode="w"))
