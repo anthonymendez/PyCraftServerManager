7814940881c595bc3069fa58f204b8f2c47f8b81
Anthony Mendez <anthonymendez9@gmail.com>
2020-09-27 21:08:09 -0700

    #17 Added comments and logging

--- PyCraftServerManager/ServerDownloader/PaperMCServerDownloader.py
+++ PyCraftServerManager/ServerDownloader/PaperMCServerDownloader.py
@@ -9,12 +9,10 @@ class PaperMCServerDownloader():
 
     def __init__(self, main_directory, server_directory, server_jars_folder):
         """
-        Initializes Vanilla Server Downloader.\n
+        Initializes Vanilla Server Downloader.
         For server_jars_folder, pass in relative location of intended server jars folder. Will create folder if it doesn't exist.
         """
         logging.info("Entry")
-        self.versions = []
-        self.download_links = {}
         self.main_directory = main_directory
         self.server_directory = server_directory
         self.server_jars = server_jars_folder
@@ -28,6 +26,13 @@ class PaperMCServerDownloader():
         logging.info("Exit")
 
     def get_version_list(self, jar_type):
+        """
+        Gets a list of available versions to download for the given jar type.
+        Example:
+        \tInput: "paper"
+        \tOutput: "['1.16.3', '1.16.2', '1.16.1', '1.15.2', '1.15.1', '1.15', '1.14.4', '1.14.3', '1.14.2', '1.14.1', '1.14', '1.13.2', '1.13.1', '1.13-pre7', '1.13', '1.12.2', '1.12.1', '1.12', '1.11.2', '1.10.2', '1.9.4', '1.8.8']"
+        """
+        logging.info("Entry")
         if jar_type == "paper":
             r = requests.post(url=self.paper_url, headers=self.head)
         elif jar_type == "waterfall":
@@ -35,15 +40,34 @@ class PaperMCServerDownloader():
         elif jar_type == "travertine":
             r = requests.post(url=self.travertine_url, headers=self.head)
         else:
+            logging.info("Exit")
             return 1
 
+        logging.info("Getting list of %s versions." % jar_type)
         paper_versions = json.loads(r.content)["versions"]
+        logging.info("Exit")
         return paper_versions
 
     def get_latest_version(self, jar_type):
-        return self.get_version_list(jar_type)[0]
+        """
+        Gets the latest version of the jar type.
+        Example:
+        \tInput: "paper"
+        \tOutput: "1.16.3"
+        """
+        logging.info("Entry")
+        latest_version = self.get_version_list(jar_type)[0]
+        logging.info("Exit")
+        return latest_version
 
     def get_builds(self, jar_type, version):
+        """
+        Gets a list of available builds for the given jar type and version.
+        Example:
+        \tInput: "paper", "1.16.3"
+        \tOutput: ['206', '205', '204', '203', '202', '201', '200', '199', '198', '197', '196', '195', '194', '193', '192', '191']
+        """
+        logging.info("Entry")
         if version == None or version == "":
             return 1
         if jar_type == None or jar_type == "":
@@ -56,15 +80,34 @@ class PaperMCServerDownloader():
         elif jar_type == "travertine":
             r = requests.post(url=self.travertine_url + "/" + version, headers=self.head)
         else:
+            logging.info("Exit")
             return 1
 
-        paper_versions = json.loads(r.content)["builds"]
+        logging.info("Getting list of builds for %s version %s." % (jar_type, version))
+        paper_versions = json.loads(r.content)["builds"]["all"]
+        logging.info("Exit")
         return paper_versions
 
     def get_build_latest(self, jar_type, version):
-        return self.get_builds(jar_type, version)["latest"]
+        """
+        Gets the latest build available for the given jar type and version.
+        Example:
+        \tInput: "paper", "1.16.3"
+        \tOutput: "206"
+        """
+        logging.info("Entry")
+        latest_build = self.get_builds(jar_type, version)[0]
+        logging.info("Exit")
+        return latest_build
 
     def download_by_jar_version_build(self, jar_type, version, build):
+        """
+        Downloads a server jar by the given jar type, version, and build.
+        Example:
+        \tInput: "paper", "1.16.3", "206"
+        \tOutput: File called "paper_1.16.3_206.jar" in server jars folder.
+        """
+        logging.info("Entry")
         if jar_type == "paper":
             url = self.paper_url
         elif jar_type == "waterfall":
@@ -72,32 +115,49 @@ class PaperMCServerDownloader():
         elif jar_type == "travertine":
             url = self.travertine_url
         else:
+            logging.info("Exit")
             return 1
 
-        # TODO: Error checking
-
+        logging.info("Downloading %s version %s build %s from %s" % (jar_type, version, build, url))
         url += "/" + version + "/" + build + "/download"
         jar_file = requests.get(url)
         jar_file_name = "%s_%s_%s.jar" % (jar_type, version, build)
         open(os.path.join(self.server_jars, jar_file_name), "wb").write(jar_file.content)
+        logging.info("Exit")
 
     def download_by_jar_version_latest(self, jar_type, version):
+        """
+        Download a server jar by the given jar type, and version, and uses the latest build.
+        Example:
+        \tInput: "paper", "1.16.3"
+        \tOutput: File called "paper_1.16.3_206.jar" in server jars folder.
+        """
+        logging.info("Entry")
         build = self.get_build_latest(jar_type, version)
-        return self.download_by_jar_version_build(jar_type, version, build)
+        to_return = self.download_by_jar_version_build(jar_type, version, build)
+        logging.info("Exit")
+        return to_return
 
     def download_by_jar_latest(self, jar_type):
+        """
+        Download a server jar by the given jar type, latest version & build.
+        Example:
+        \tInput: "paper"
+        \tOutput: File called "paper_1.16.3_206.jar" in server jars folder.
+        """
+        logging.info("Entry")
         version = self.get_latest_version(jar_type)
-        return self.download_by_jar_version_latest(jar_type, version)
+        to_return = self.download_by_jar_version_latest(jar_type, version)
+        logging.info("Exit")
+        return to_return
 
-print(PaperMCServerDownloader("","","").get_version_list("paper"))
-print("")
-print(PaperMCServerDownloader("","","").get_latest_version("paper"))
-print("")
-print(PaperMCServerDownloader("","","").get_builds("paper", "1.16.3"))
-print("")
-print(PaperMCServerDownloader("","","").get_build_latest("paper", "1.16.3"))
+## Example on how to use
+# print(PaperMCServerDownloader("","","").get_version_list("paper"))
+# print(PaperMCServerDownloader("","","").get_latest_version("paper"))
+# print(PaperMCServerDownloader("","","").get_builds("paper", "1.16.3"))
+# print(PaperMCServerDownloader("","","").get_build_latest("paper", "1.16.3"))
 
-PaperMCServerDownloader("","","").download_by_jar_version_build("paper", "1.16.3", "191")
-PaperMCServerDownloader("","","").download_by_jar_version_latest("paper", "1.16.3")
-PaperMCServerDownloader("","","").download_by_jar_latest("waterfall")
-PaperMCServerDownloader("","","").download_by_jar_latest("travertine")
+# PaperMCServerDownloader("","","").download_by_jar_version_build("paper", "1.16.3", "191")
+# PaperMCServerDownloader("","","").download_by_jar_version_latest("paper", "1.16.3")
+# PaperMCServerDownloader("","","").download_by_jar_latest("waterfall")
+# PaperMCServerDownloader("","","").download_by_jar_latest("travertine")
